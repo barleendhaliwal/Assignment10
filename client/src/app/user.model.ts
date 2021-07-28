355f1ea803702625e033daf998b09a17903bb01b
Barleen Dhaliwal <barleen.dhaliwal@sourcefuse.com>
2021-07-28 11:53:07 +0530

    Added all client and server side files

---
+++ client/src/app/user.model.ts
@@ -0,0 +1,12 @@
+export class User  {
+  //need to initialize in strict mode
+    id: number=-1;
+    firstName: string='';
+    middleName?: string;
+    lastName: string='';
+    email: string='';
+    phoneNumber: string='';
+    role: string='';
+    address: string='';
+    customerName: string='';
+  }
