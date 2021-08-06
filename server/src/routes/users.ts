622dd569d3811d1ac51d438e940c07e8829ac7d3
Barleen Dhaliwal <barleen.dhaliwal@sourcefuse.com>
2021-08-06 15:50:23 +0530

    Updated server side according to naming conventions and appropriate http codes, used environment variables both for server and angular, removed log statements, removed dist folder

--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -1,40 +1,40 @@
 import express, { Request, Response } from "express";
+import * as dotenv from 'dotenv';
 
-import { Pool } from "pg";
+dotenv.config({path:__dirname+'/../../.env'});
+
 const router = express.Router();
+const Pool = require('pg').Pool
 
-
-type T = {
+type User = {
 
     firstName: string;
     middleName: string;
     lastName: string;
     email: string;
     phoneNumber: string;
-    role: string;
+    role: number;
     address: string;
     customerName: string;
 }
 
-// const Pool = require('pg').Pool
+
 const pool = new Pool({
-    user: 'barleen',
-    host: 'localhost',
-    database: 'barleen',
-    password: '123456',
-    port: 5432,
+    user: process.env.USER_NAME,
+    host: process.env.DATABASE_HOST,
+    database: process.env.DATABASE,
+    password: process.env.PASSWORD,
+    port: process.env.PORT,
 })
 
 //SENDS ALL MEMBERS
 router.get("/", (req: Request, res: Response) => {
-
-    const query = `SELECT E.id, E."firstName", E."middleName", E."lastName", E.email, E."phoneNumber", E.address, E.name as "customerName", role.name as role FROM (SELECT users.*, customers.name  FROM users INNER JOIN customers ON users."customerID"=customers.id) as E INNER JOIN role on E.role=role.key ORDER BY E.id;`
-
-    pool.query(query, (err, result) => {
+    //no need to change to query as it is read operation and createdOn and modifiedOn is not to be displayed on client side
+    const query = `SELECT E.id, E."firstName", E."middleName", E."lastName", E.email, E."phoneNumber", E.address, E.role,E.name as "customerName" FROM (SELECT users.*, customers.name  FROM users INNER JOIN customers ON users."customerId"=customers.id) as E  ORDER BY E.id;`
+    pool.query(query, (err:any, result:any) => {
         if (err) {
             throw err
         }
-        console.log(result.rows)
         res.status(200).json(result.rows)
     })
 
@@ -44,14 +44,14 @@ router.get("/", (req: Request, res: Response) => {
 //SEND A SPECIFIC MEMEBER 
 router.get("/:id", (req: Request, res: Response) => {
 
-
+    
     let id = req.params.id;
-    const query = `SELECT E.id, E."firstName", E."middleName", E."lastName", E.email, E."phoneNumber", E.address, E.name as "customerName", role.name as role FROM (SELECT users.*, customers.name  FROM users INNER JOIN customers ON users."customerID"=customers.id) as E INNER JOIN role on E.role=role.key where E.id = ${id}; `
-    pool.query(query, (err, result) => {
+    //no need to change to query as it is read operation and createdOn and modifiedOn is not to be displayed on client side
+    const query = `SELECT E.id, E."firstName", E."middleName", E."lastName", E.email, E."phoneNumber", E.address, E.role,E.name as "customerName" FROM (SELECT users.*, customers.name  FROM users INNER JOIN customers ON users."customerId"=customers.id) as E where E.id=${id}; `
+    pool.query(query, (err:any, result:any) => {
         if (err) {
             throw err
         }
-        console.log(result.rows)
         res.status(200).json(result.rows)
     })
 
@@ -60,8 +60,7 @@ router.get("/:id", (req: Request, res: Response) => {
 //ADD MEMBER
 router.post("/", (req: Request, res: Response) => {
 
-    console.log("add member")
-    const newMember: T = {
+    const newMember: User = {
 
         firstName: req.body.firstName,
         middleName: req.body.middleName,
@@ -74,60 +73,63 @@ router.post("/", (req: Request, res: Response) => {
 
 
     }
+  
 
-
-    if (!newMember.firstName || !newMember.lastName || !newMember.email || !newMember.phoneNumber || !newMember.role || !newMember.address) {
-        console.log(newMember)
+    if (!newMember.firstName || !newMember.lastName || !newMember.email || !newMember.phoneNumber || newMember.role<0||newMember.role>2 || !newMember.address||!newMember.customerName) {
         res.status(400).json({ message: `Give Correct Input` })
+        return;
+  
     }
     if (newMember.phoneNumber.length !== 10) {
-        res.status(400).json({ message: `Give Correct Input` })
+        res.status(400).json({ message: `Phone Number must be 10 digits` })
+        return;
     }
     else {
 
-        pool.query(`SELECT id FROM users where "phoneNumber" ='${newMember.phoneNumber}'`, (err, result) => {
+        pool.query(`SELECT id FROM users where "phoneNumber" ='${newMember.phoneNumber}'`, (err:any, result:any) => {
             if (err) {
                 throw err
             }
             if (result.rows.length !== 0) {
-                console.log(result);
+                //400 - client error - sent invalid user information
                 res.status(400).json({ message: `User Already Exists` })
+                return;
             }
             else {
 
-                const queryValidationRole = `SELECT key from role where LOWER(name)=LOWER('${newMember.role}');`;
-                console.log(queryValidationRole);
-                pool.query(queryValidationRole, (err, resultRole) => {
+                const queryValidationRole = `SELECT key from role where key =${newMember.role};`;
+                pool.query(queryValidationRole, (err:any, resultRole:any) => {
                     if (err) {
                         throw err;
                     }
                     if (resultRole.rows.length == 0) {
-
+                        //400 - client error - sent invalid role
                         res.status(400).json({ message: `User Role does not exist !` })
+                        return;
+                      
                     }
                     else {
 
-                        //console.log(result_outer.rows[0].key);
-
                         const queryValidationCustomer = `SELECT id from customers where LOWER(name)=LOWER('${newMember.customerName}');`;
-                        console.log(queryValidationCustomer);
-                        pool.query(queryValidationCustomer, (err, resultCustomer) => {
+                        pool.query(queryValidationCustomer, (err:any, resultCustomer:any) => {
                             if (err) {
                                 throw err;
                             }
                             if (resultCustomer.rows.length == 0) {
-
+                                //400 - client error - sent invalid customer
                                 res.status(400).json({ message: `Customer does not exist !` })
+                                return;
+                                
                             }
                             else {
-
-                                const query = `INSERT into users ("firstName", "middleName", "lastName", email, "phoneNumber", role, address, "customerID") VALUES ('${newMember.firstName}','${newMember.middleName}','${newMember.lastName}','${newMember.email}','${newMember.phoneNumber}',${resultRole.rows[0].key},'${newMember.address}','${resultCustomer.rows[0].id}');`;
+                                //MODIFIED QUERY
+                                const query = `INSERT into users ("firstName", "middleName", "lastName", email, "phoneNumber", role, address, "customerId", "createdOn") VALUES ('${newMember.firstName}','${newMember.middleName}','${newMember.lastName}','${newMember.email}','${newMember.phoneNumber}',${newMember.role},'${newMember.address}','${resultCustomer.rows[0].id}',CURRENT_TIMESTAMP);`;
                                 pool.query(query, (err: any, result: any) => {
                                     if (err) {
                                         throw err
                                     }
 
-                                    res.status(200).json({ message: `Added User Successfully !`, addedRecord: newMember })
+                                    res.status(201).json({ message: `Added User Successfully !`, addedRecord: newMember })
                                 })
                             }
 
@@ -147,13 +149,14 @@ router.post("/", (req: Request, res: Response) => {
 router.put('/:id', (req: Request, res: Response) => {
     let id = req.params.id;
 
-    pool.query(`SELECT * FROM users where id =${id}`, (err, result) => {
+    pool.query(`SELECT * FROM users where id =${id}`, (err:any, result:any) => {
         if (err) {
             throw err
         }
         if (result.rows.length === 0) {
 
-            res.status(404).json({ message: `User Does Not Exists` })
+            res.status(404).json({ message: `User Does Not Exists`, success:0 }) //404 - Requested for resource which doesn't exist
+            return;
         }
         else {
             let firstName = req.body.firstName;
@@ -165,37 +168,37 @@ router.put('/:id', (req: Request, res: Response) => {
             let address = req.body.address;
             let customerName = req.body.customerName;
             if (phoneNumber.length !== 10) {
-                res.status(400).json({ message: `Give Correct Input` })
+                res.status(400).json({ message: `Phone Number must be 10 digits`, success:0 })
+                return;
 
             }
             else {
 
-                const queryValidationRole = `SELECT key from role where LOWER(name)=LOWER('${role}');`;
-                console.log(queryValidationRole);
-                pool.query(queryValidationRole, (err, resultRole) => {
+                const queryValidationRole = `SELECT key from role where key=${role};`;
+                pool.query(queryValidationRole, (err:any, resultRole:any) => {
                     if (err) {
                         throw err;
                     }
                     if (resultRole.rows.length == 0) {
-
-                        res.status(400).json({ message: `User Role does not exist !` })
+                        //400 - client error - sent invalid role
+                        res.status(400).json({ message: `User Role does not exist !`, success:0})
+                        return;
                     }
                     else {
                         const queryValidationCustomer = `SELECT id from customers where LOWER(name)=LOWER('${customerName}');`;
-                        console.log(queryValidationCustomer);
-                        pool.query(queryValidationCustomer, (err, resultCustomer) => {
+                        pool.query(queryValidationCustomer, (err:any, resultCustomer:any) => {
                             if (err) {
                                 throw err;
                             }
                             if (resultCustomer.rows.length == 0) {
-
-                                res.status(400).json({ message: `Customer does not exist !` })
+                                //400 - client error - sent invalid customer
+                                res.status(400).json({ message: `Customer does not exist !`,success:0 })
+                                return;
                             }
                             else {
-                                const query = `UPDATE users SET "firstName"='${firstName}', "middleName"='${middleName}', "lastName"='${lastName}', email='${email}', "phoneNumber"='${phoneNumber}', role='${resultRole.rows[0].key}', address='${address}', "customerID"='${resultCustomer.rows[0].id}' where id=${id};`;
-
-                                console.log(query);
-                                pool.query(query, (err, result) => {
+                                //MODIFIED QUERY
+                                const query = `UPDATE users SET "firstName"='${firstName}', "middleName"='${middleName}', "lastName"='${lastName}', email='${email}', "phoneNumber"='${phoneNumber}', role=${role}, address='${address}', "customerId"='${resultCustomer.rows[0].id}', "modifiedOn"=CURRENT_TIMESTAMP where id=${id};`;
+                                pool.query(query, (err:any, result:any) => {
                                     if (err) {
                                         throw err
                                     }
@@ -210,7 +213,8 @@ router.put('/:id', (req: Request, res: Response) => {
                                             role: role,
                                             address: address,
                                             customerName: customerName
-                                        }
+                                        },
+                                        success:1
                                     })
 
                                 })
@@ -234,13 +238,13 @@ router.delete('/:id', (req: Request, res: Response) => {
             throw error
         }
         if (result.rows.length === 0) {
-
-            res.status(400).json({ message: `User Does Not Exists` })
+            //404 - request for a resource that does not exist
+            res.status(404).json({ message: `User Does Not Exists` })
+            return;
         }
         else {
 
             const query = `DELETE from users where id=${id};`;
-            console.log(query);
             pool.query(query, (error: any, result: any) => {
                 if (error) {
                     throw error
